e3584248922556ec7e0a3354ce5eea91ab95dc22
heokim <andresssshk@gmail.com>
2022-08-04 13:57:04 -0400

    Ajuste de tabulacion

--- src/componets/Componente.js
+++ src/componets/Componente.js
@@ -4,30 +4,30 @@ import React from "react";
 // no se usa mucho apartir de la llegade de los Hooks
 /*
 class Componente extends Component {
-    render() {
-        return <h2>{this.props.msg + "Soy un componente basado en Clase"}</h2>
-    }
+  render() {
+    return <h2>{this.props.msg + "Soy un componente basado en Clase"}</h2>
+  }
 }
 */
 
 // Componente funcional
 /*
 function Componente(props) {
-    return (
-        <h2>
-            {props.msg + "Funcional basado en Funcion explicita"}
-        </h2>
-    );
+  return (
+    <h2>
+      {props.msg + "Funcional basado en Funcion explicita"}
+    </h2>
+  );
 }
 */
 
 // con funcion no declarada, Expresada
 const Componente = props => {
-    return (
-        <h2>
-            {props.msg + "Funcional expresado desde una prop"}
-        </h2>
-    );
+  return (
+    <h2>
+      {props.msg + "Funcional expresado desde una prop"}
+    </h2>
+  );
 };
 
 export default Componente;
