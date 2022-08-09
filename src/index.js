cd601b8490cbf3401962ac306e875a5adae9617b
heokim <andresssshk@gmail.com>
2022-08-09 09:18:59 -0400

    cambio de comentario al index

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-{/*
+
 // para React v18
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-    <App />
-    </React.StrictMode>
-);
- */}
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//     <App />
+//     </React.StrictMode>
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
